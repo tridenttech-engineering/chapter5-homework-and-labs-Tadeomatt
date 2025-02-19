176f1311c6847de3a24970c82a538d86ce5870b8
Tadeomatt <tadeomatt4@gmail.com>
2025-02-19 00:35:57 +0000

    Submission for Hw5-12

--- .ccls-cache/@home@runner@workspace/hw5-12.cpp
+++ .ccls-cache/@home@runner@workspace/hw5-12.cpp
@@ -7,6 +7,26 @@ using namespace std;
 
 int main()
 {
+	double price1, price2, total, lowerPrice;
+
+	cout << "Enter the price of item 1: ";
+	cin >> price1;
+	cout << "Enter the price of item 2: ";
+	cin >> price2;
+
+	//Determin the lower price
+	if (price1 < price2) {
+		lowerPrice = price1;
+	}
+	else {
+		lowerPrice = price2;
+	}
+	//calculate total with half off on the lower price
+	total = price1 + price2 - (lowerPrice / 2);
+
+	cout << fixed << setprecision(2);
+	cout << "Total owed: $" << total << endl;
+
 
 	return 0;
 }	//end of main function
